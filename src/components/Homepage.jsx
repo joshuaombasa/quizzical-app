e29a8bc29a3fab81824be20bbf153a45d35367d6
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-02 20:43:47 +0300

    added Homepage component

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Homepage() {
+    return (
+        <main className="homepage">
+            <div className="home--container">
+                <h1 className="title">Quizzical</h1>
+                <button className="start-quiz-btn">Start quiz</button>
+            </div>
+        </main>
+    )
+}

76dc9fcc972288ada2dbcdd31b84a1d8d89849f0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-02 21:16:29 +0300

    updated jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Homepage() {
+export default function Homepage(props) {
     return (
         <main className="homepage">
             <div className="home--container">
                 <h1 className="title">Quizzical</h1>
-                <button className="start-quiz-btn">Start quiz</button>
+                <button className="start-quiz-btn" onClick={props.startQuiz} >Start quiz</button>
             </div>
         </main>
     )

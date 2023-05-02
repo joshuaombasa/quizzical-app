f875bd2f1b837900031d6212bd25fcdc39818b24
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-02 22:06:29 +0300

    updated jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,16 @@ import Quiz from './Quiz'
 import '../styles/App.css'
 
 
-
 function App() {
   const [isQuizStarted, setIsQuizStarted] = React.useState(false)
+
+  const [quizData, setQuizData] = React.useState()
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple")
+        .then(res => res.json())
+        .then(data => console.log(data.results[0]))
+  })
 
   function startQuiz() {
     setIsQuizStarted(prevIsQuizStarted => !prevIsQuizStarted)

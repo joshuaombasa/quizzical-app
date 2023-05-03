1490c1fcce8494309ff52529b589a954c8ab90ec
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-03 22:23:26 +0300

    updated jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,23 @@
 import React from 'react' 
+import uuid from 'react-uuid';
 
 import Question from './Question'
 
-export default function Quiz() {
+export default function Quiz(props) {
+
+    const questionsJsx = props.quizData.map((item) => {
+        return  <Question 
+                    questionText={item.question} 
+                    id={uuid()} 
+                    key={uuid()}
+                    answers={item.incorrect_answers}
+                    correctAnswer={item.correct_answer}
+                />
+    })
+
     return (
         <div className="quiz--container">
-            <Question />
+            {questionsJsx}
         </div>
     )
 }

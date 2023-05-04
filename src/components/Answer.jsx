1730d468f4b25743dc791f6d7caac333b0fb8f34
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-04 23:40:16 +0300

    updated jsx

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
 export default function Answer(props) {
-    const [isClicked, setIsClicked] = React.useState(false)
-
     
 
+    const correctAnswerId = props.correctAnswerId
+
     function handleClick() {
-        setIsClicked((prevIsClicked) => !prevIsClicked)
+       
        
     }
 
-    const styles = {backgroundColor: isClicked? "#94D7A2": "white"}
+    // const styles = {backgroundColor: correctAnswerId === props.id ? "#94D7A2": "white"}
 
     
     return (
-        <div style={styles} className="answer-btn" onClick={handleClick}>
+        <div style={styles} className="answer-btn" onClick={() => {}}>
             <small className='btn-text'>{props.answerText}</small>
         </div>
     )

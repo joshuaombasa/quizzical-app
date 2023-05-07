a61ff4385c6110f2261632083f225d8e87235ef7
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-07 14:45:59 +0300

    updated jsx

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -2,11 +2,34 @@ import React from 'react'
 
 export default function Answer(props) {
     
-//    console.log(props.isChosen)
+     
+    let styles 
     
-    const styles = {backgroundColor: props.isChosen ? "#D6DBF5": "white"}
-   
+    if (props.isAnwered) {
+         if (props.isChosen && props.id === props.questionId) {
+            styles = {backgroundColor: "green", color: "white"}
+         } else if(props.isChosen && props.id !== props.questionId) {
+            styles =  {backgroundColor: "red", color: "white"}
+         } else if(!props.isChosen && props.id === props.questionId) {
+            styles =  {backgroundColor: "green", color: "white"}
+         }
+    } else {
+        if (props.isChosen) {
+           styles = {backgroundColor: "#D6DBF5"}
+        } else {
+           styles =  {backgroundColor: "white"}
+        }
+    }
 
+    // const styles = props.isAnwered 
+    // ? 
+    // {backgroundColor: 
+    //     props.isChosen ? "red":
+    //     props.id === props.questionId ? "green" : ''  }
+    // :{backgroundColor: props.isChosen ? "#D6DBF5": "white"}
+   
+    
+   
     
     return (
         <div  

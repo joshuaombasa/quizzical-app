e2e36a83fcf6b8e47b80bb16111a7ee44543d695
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-07 17:55:45 +0300

    updated functionality to restart game

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import React from 'react' 
+import React from 'react'
 import uuid from 'react-uuid';
 
 import Question from './Question'
@@ -7,28 +7,39 @@ import Question from './Question'
 
 export default function Quiz(props) {
 
-   
+
 
     const questionsJsx = props.quizData.map((item) => {
-        return  <Question 
-                    questionText={item.question} 
-                    isAnwered={item.isAnwered} 
-                    questionId={item.id} 
-                    key={item.id}
-                    answers={item.answers}
-                    correctAnswer={item.correctAnswer}
-                    toggleChosenAnswer={props.toggleChosenAnswer}
-                />
+        return <Question
+            questionText={item.question}
+            isAnwered={item.isAnwered}
+            questionId={item.id}
+            key={item.id}
+            answers={item.answers}
+            correctAnswer={item.correctAnswer}
+            toggleChosenAnswer={props.toggleChosenAnswer}
+        />
     })
 
-   
+
 
     return (
         <div className="quiz--container">
             {questionsJsx}
             <div className="bottom-container">
-            {props.isQuizOver && <h3 className='score-message'>You scored {props.scoreCount}/10 corrrect answers</h3>}
-            <button className="quiz-btn" onClick={props.questionIsAnsweredToggle}>Check answers</button>
+                {props.isQuizOver && <h3 className='score-message'>You scored {props.scoreCount}/10 corrrect answers</h3>}
+                {
+                    props.isplayAgain ?
+                        <button className="quiz-btn" onClick={props.startQuiz}>
+                            Play again
+                        </button>
+                        : <button className="quiz-btn" onClick={props.questionIsAnsweredToggle}>
+                            Check answers
+                        </button>
+
+                }
+
+
             </div>
         </div>
     )

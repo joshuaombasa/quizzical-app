267f8a63b2a4541ee4f74a448fd7df718b59bec0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-07 12:45:37 +0300

    updated jsx

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 export default function Answer(props) {
     
-
+//    console.log(props.isChosen)
     
-    // const styles = {backgroundColor: isAnswered ? "#D6DBF5": "white"}
+    const styles = {backgroundColor: props.isChosen ? "#D6DBF5": "white"}
    
 
     
     return (
-        <div  className="answer-btn" >
+        <div  
+        style={styles}
+        className="answer-btn" 
+        onClick={() => props.toggleChosenAnswer(props.id, props.questionId)}>
             <small className='btn-text'>{props.answerText}</small>
         </div>
     )

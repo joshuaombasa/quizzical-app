d9e635c476d0f954968c44cf08ddfdf2cbb781e8
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-07 12:48:18 +0300

    updated jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,48 +11,67 @@ function App() {
 
   const [quizData, setQuizData] = React.useState()
 
-  
+
 
   React.useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple")
-        .then(res => res.json())
-        .then(data => {
-          const quizDataWithIds = data.results.map(item => {
-            return {
-              ...item,
-              question: item.question,
-              isAnwered: false,
-              id: uuid(),
-              correctAnswer: item.correct_answer,
-              answers: [...item.incorrect_answers, item.correct_answer]
-            }
-          })
+      .then(res => res.json())
+      .then(data => {
+        const quizDataWithIds = data.results.map(item => {
+          return {
 
-          const updatedQuizData =  quizDataWithIds.map(item => {
-            const updatedAnswersData = item.answers.map(ans => {
-              return {
-                value: ans,
-                id: uuid(),
-                isChosen: false
-              }
-            })
-
-            return {
-              ...item,
-              answers: updatedAnswersData
-            }
-          })
-
-
-
-          setQuizData(updatedQuizData)
-
-          console.log(data.results[0])
-          
+            question: item.question,
+            isAnwered: false,
+            id: uuid(),
+            correctAnswer: item.correct_answer,
+            answers: [...item.incorrect_answers, item.correct_answer]
+          }
         })
-  },[])
 
-//  console.log(quizData)
+        const updatedQuizData = quizDataWithIds.map(item => {
+          const updatedAnswersData = item.answers.map(ans => {
+            return {
+              value: ans,
+              id: uuid(),
+              isChosen: false
+            }
+          })
+
+          return {
+            ...item,
+            answers: updatedAnswersData
+          }
+        })
+
+
+
+        setQuizData(updatedQuizData)
+
+        // console.log(data.results[0])
+
+      })
+  }, [])
+
+  
+
+  function toggleChosenAnswer(answerId, questionId) {
+    setQuizData(prevQuizData => {
+
+      return prevQuizData.map(item => {
+        return item.id === questionId ? {...item, 
+          answers: 
+          item.answers.map(ans => {
+            return ans.id === answerId ? {...ans, isChosen: !ans.isChosen} : {...ans, isChosen: false}
+          })} 
+        : item
+      })
+    })
+
+
+
+
+
+  }
 
   function startQuiz() {
     setIsQuizStarted(prevIsQuizStarted => !prevIsQuizStarted)
@@ -60,17 +79,17 @@ function App() {
 
   return (
     <>
-      {isQuizStarted === false 
-      ?
-      <Homepage startQuiz={startQuiz} 
-      /> 
-      :
-      <Quiz 
-        quizData={quizData}
-      />}
+      {isQuizStarted === false
+        ?
+        <Homepage startQuiz={startQuiz}
+        />
+        :
+        <Quiz
+          quizData={quizData}
+          toggleChosenAnswer={toggleChosenAnswer}
+        />}
     </>
   )
 }
 
 export default App
- 
